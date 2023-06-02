c79dd28b95c5ddf83236887a55d166862368187c
IdalisNguyen <xitet0007@gmail.com>
2023-06-02 10:33:32 +0700

    luu ngay 2-6-2003

--- style.js
+++ style.js
@@ -468,3 +468,25 @@ function logOut(){
 function logIn(){
     cur_account='admin';
 }
+
+
+//them
+const body = document.querySelector("body"),
+    modeToggle = body.querySelector(".mode-toggle");
+sidebar = body.querySelector("sidebar");
+sidebarToggle = body.querySelector(".sidebar-toggle");
+
+
+let getStatus = localStorage.getItem("status");
+if (getStatus && getStatus === "close") {
+    sidebar.classList.toggle("close");
+}
+
+sidebarToggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+    if (sidebar.classList.contains("close")) {
+        localStorage.setItem("status", "close");
+    } else {
+        localStorage.setItem("status", "open");
+    }
+})
